68bbdb12fc6bcf46e9ead5120c9e8f430981b28a
hofifahisma <55371722+hofifahisma@users.noreply.github.com>
2019-09-19 18:39:40 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,23 @@
+<h1>Luas Segitiga</h1>
+<?php
+echo "L = 1/2 x alas x tinggi";
+echo "<br>";
+echo "L = 1/2 x 16 x 8 = ";
+echo 1/2 * 16 * 8;
+?>
+
+<h1>Luas Trapesium</h1>
+<?php
+echo "L = alas x tinggi";
+echo "<br>";
+echo "L = 15 x 6 = ";
+echo 15 * 6;
+?>
+
+<h1>Luas Lingkaran</h1>
+<?php
+echo "L = πr^2"; 
+echo "<br>";
+echo "L = 22/7 x 14 X 14 = ";
+echo 22/7 * 14 * 14;
+?>
